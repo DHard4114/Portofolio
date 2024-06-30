d80589aa4cdfa09aaa6a153181231f0cf0109fe7
unknown <dapahardan@gmail.com>
2024-06-30 15:31:32 +0700

    First

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html",
+    "node_modules/preline/dist/*.js",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("preline/plugin"), require("flowbite/plugin")],
+};
