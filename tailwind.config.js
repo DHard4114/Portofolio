4b83cae6a0ee932868f5c271fe5223b9aa24eba2
unknown <dapahardan@gmail.com>
2024-07-03 20:02:02 +0700

    Day-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./*.html", "./js/**/*.js"],
   theme: {
     container: {
       center: true,
